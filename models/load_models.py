d47345c3448e66e90b5fc12e244a553614b4a7e8
Michal Shlapentokh- Rothman <michalshlapentokh-rothman@MacBook-Pro.local>
2020-11-16 04:05:26 -0500

    models

--- models/load_models.py
+++ models/load_models.py
@@ -5,7 +5,7 @@ from torchvision import datasets, models, transforms
 
 def load_model(model_type,num_classes,pretrained=True,from_file=False):
     fine_tune_needed = False
-    model = models.model_type(pretrained=pretrained)
+    model = models.resnet50(pretrained=pretrained)
     if model.fc.in_features != num_classes:
         model.fc = nn.Linear(model.fc.in_features,num_classes)
         fine_tune_needed = True
