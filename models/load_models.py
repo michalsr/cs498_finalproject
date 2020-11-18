c55aec21ff8dc44839f26b6f63c78612584aee6a
Michal Shlapentokh-Rothman <michal5@vision-15.cs.illinois.edu>
2020-11-17 23:19:38 -0600

    added muco

--- models/load_models.py
+++ models/load_models.py
@@ -3,9 +3,22 @@ import torch.nn as nn
 import torchvision
 from torchvision import datasets, models, transforms
 
-def load_model(model_type,num_classes,pretrained=True,from_file=False):
+import torch.utils.model_zoo as model_zoo
+def load_model(model_type,num_classes,pretrained,from_file=False):
     fine_tune_needed = False
-    model = models.resnet50(pretrained=pretrained)
+    if pretrained == 'ImageNet':
+        model = models.resnet50(pretrained=True)
+    else:
+        model = models.resnet50()
+    if pretrained == 'moco':
+        checkpoint = model_zoo.load_url(
+            'https://dl.fbaipublicfiles.com/moco/moco_checkpoints/moco_v2_800ep/moco_v2_800ep_pretrain.pth.tar')
+        pre_dict = {str.replace(k, 'module.encoder_q.', ''): v for k, v in checkpoint['state_dict'].items()}
+        state_dict = model.state_dict()
+        pretrained_dict = {k: v for k, v in pre_dict.items() if k in state_dict}
+        state_dict.update(pretrained_dict)
+        model.load_state_dict(state_dict)
+
     if model.fc.in_features != num_classes:
         model.fc = nn.Linear(model.fc.in_features,num_classes)
         fine_tune_needed = True
